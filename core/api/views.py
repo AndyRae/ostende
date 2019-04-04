7a4bffde8cd9604b4dc08fcd79e3971575458a94
AndyRae <hello@rae.li>
2019-04-04 01:03:35 +0100

    bootstrap styling

--- core/api/views.py
+++ core/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, filters, generics
 from core.models import Screening
 from datetime import datetime
 from .serializers import ScreeningSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
 class ScreeningViewSet(viewsets.ModelViewSet):
@@ -9,4 +10,5 @@ class ScreeningViewSet(viewsets.ModelViewSet):
     queryset = Screening.objects.filter(date__gte=todaysdate).order_by('date')
     serializer_class = ScreeningSerializer
     filter_backends = (filters.SearchFilter,)
+    # pagination_class = PageNumberPagination
     search_fields = ('name', 'film__name', 'venue__name', 'venue__city', 'season__name',)
