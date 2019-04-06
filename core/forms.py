8fc9ac6d140926a657d3d44df85773933e46b346
AndyRae <hello@rae.li>
2019-04-06 01:56:13 +0100

    articles model

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from tinymce import TinyMCE
 from .models import Venue, Film, Season, Article
 
 
@@ -24,3 +25,20 @@ class articleuploadform(forms.ModelForm):
     class Meta:
         model = Article
         fields = ('image',)
+
+
+class TinyMCEWidget(TinyMCE):
+    def use_required_attribute(self, *args):
+        return False
+
+
+class ArticleForm(forms.ModelForm):
+    text = forms.CharField(
+        widget=TinyMCEWidget(
+            attrs={'required': False, 'cols': 30, 'rows': 10}
+        )
+    )
+
+    class Meta:
+        model = Article
+        fields = '__all__'
