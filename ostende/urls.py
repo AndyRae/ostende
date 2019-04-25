18e3e574f8ac779dac2bf7abebd6dee372f1971b
AndyRae <hello@rae.li>
2019-04-25 23:09:59 +0100

    flatpages

--- ostende/urls.py
+++ ostende/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.contrib.flatpages import views
 from .routers import router
 
 urlpatterns = [
@@ -26,6 +27,8 @@ urlpatterns = [
     path('', include('core.urls')),
     path(r'tinymce/', include('tinymce.urls')),
     path('api/', include(router.urls)),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('about/', views.flatpage, {'url': '/about/'}, name='about'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
