3524c255675052062afd660e7902564ab2903457
AndyRae <hello@rae.li>
2019-06-09 19:16:02 +0100

    screenings order by time, and articles pinned

--- core/api/views.py
+++ core/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class ScreeningViewSet(viewsets.ModelViewSet):
     todaysdate = datetime.now().date()
-    queryset = Screening.objects.filter(date__gte=todaysdate).order_by('date')
+    queryset = Screening.objects.filter(date__gte=todaysdate).order_by('date', 'start_time')
     serializer_class = ScreeningSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     search_fields = ('name', 'film__name', 'venue__name', 'venue__city', 'venue__county', 'season__name', 'date',)
@@ -19,7 +19,7 @@ class ScreeningDateViewSet(viewsets.ModelViewSet):
     d = timedelta(days=30)
     todaysdate = datetime.now().date()
     targetdate = datetime.now().date() + d
-    queryset = Screening.objects.filter(date__gte=todaysdate).filter(date__lte=targetdate).order_by('date')
+    queryset = Screening.objects.filter(date__gte=todaysdate).filter(date__lte=targetdate).order_by('date', 'start_time')
     serializer_class = ScreeningSerializer
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     search_fields = ('name', 'film__name', 'venue__name', 'venue__city', 'season__name', 'date',)
